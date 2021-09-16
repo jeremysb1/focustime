0ded0f3fa5cc7dafb915c0379c480764389d3c08
Jeremy Hall <21281011+jeremysb1@users.noreply.github.com>
2021-09-16 11:59:46 -0400

    adding countdown timer display

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+
 import { colors } from '../../utils/colors';
 import { spacing } from '../../utils/sizes';
+import { Countdown } from '../../components/Countdown';
 
 export const Timer = ({ focusSubject }) => {
   return (
     <View style={styles.container}>
+      <Countdown />
       <View style={{ paddingTop: spacing.xxl }}>
         <Text style={styles.title}>Focusing on:</Text>
         <Text style={styles.task}>{focusSubject}</Text>
