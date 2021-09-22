addd10300c39aa968bb82f31f29d97aef666ecae
Jeremy Hall <21281011+jeremysb1@users.noreply.github.com>
2021-09-22 13:14:57 -0400

    adding lifecycle functions to store history

--- App.js
+++ App.js
@@ -1,23 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Platform } from 'react-native';
 import { Focus } from './src/features/focus/Focus';
 import { Timer } from './src/features/timer/Timer';
 import { colors } from './src/utils/colors';
 import { spacing } from './src/utils/sizes';
 
-export default function App() {
+const STATUSES = {
+  COMPLETE: 1,
+  CANCELLED: 2,
+};
 
+export default function App() {
   const [focusSubject, setFocusSubject] = useState(null);
+  const [focusHistory, setFocusHistory] = useState([]);
+
+  const addFocusHistorySubjectWithState = (subject, status) => {
+    setFocusHistory([...focusHistory, { subject, status }]);
+  };
 
   return (
     <View style={styles.container}>
       {focusSubject ? (
-        <Timer focusSubject={focusSubject} onTimerEnd={() => {
-          setFocusSubject(null);
-        }} 
+        <Timer
+          focusSubject={focusSubject}
+          onTimerEnd={() => {
+            addFocusHistorySubjectWithState(focusSubject, STATUSES.COMPLETE);
+            setFocusSubject(null);
+          }}
+          clearSubject={() => {
+            addFocusHistorySubjectWithState(focusSubject, STATUSES.CANCELLED);
+            setFocusSubject(null);
+          }}
         />
       ) : (
-        <Focus addSubject={setFocusSubject}/>
+        <Focus addSubject={setFocusSubject} />
       )}
     </View>
   );
